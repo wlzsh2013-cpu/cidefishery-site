644ac980659183e09380f96715da8d59e38d3425
wlzsh2013-cpu <wlzsh2013@gmail.com>
2026-07-03 16:39:41 +0800

    Fix-base-path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,18 @@
-// @ts-check import { defineConfig } from 'astro/config'; import sitemap from '@astrojs/sitemap';  export default defineConfig({   site: 'https://cidefishery.com',   integrations: [sitemap()],   output: 'static',   build: {     assets: 'assets',   },   vite: {     build: {       cssMinify: false,     },   }, });
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://wlzsh2013-cpu.github.io',
+  integrations: [sitemap()],
+  base: '/cidefishery-site/',
+  output: 'static',
+  build: {
+    assets: 'assets',
+  },
+  vite: {
+    build: {
+      cssMinify: false,
+    },
+  },
+});
